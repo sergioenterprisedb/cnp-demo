979e322a7f4b692e52236da5b31216bfa34a082b
Sergio Romera <sergio.romera@enterprisedb.com>
2022-05-12 11:09:28 +0200

    Version 1.0

--- 01_install_plugin.sh
+++ 01_install_plugin.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl -sSfL \
-  https://github.com/cloudnative-pg/cloudnative-pg/raw/main/hack/install-cnpg-plugin.sh | \
-  sh -s -- -b /usr/local/bin
+#curl -sSfL \
+#  https://github.com/cloudnative-pg/cloudnative-pg/raw/main/hack/install-cnpg-plugin.sh | \
+#  sh -s -- -b /usr/local/bin
 
+./download/install-cnpg-plugin.sh -b /usr/local/bin
--- 02_install_operator.sh
+++ 02_install_operator.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-kubectl apply -f \
-  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/releases/cnpg-1.15.0.yaml
+#kubectl apply -f \
+#  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/releases/cnpg-1.15.0.yaml
+
+kubectl apply -f ./download/cnpg-1.15.0.yaml
