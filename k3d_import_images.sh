ae15ea45a4ccf7eabe0f16ad28e520ce9cc13943
Sergio Romera <sergio.romera@enterprisedb.com>
2022-05-18 15:28:45 +0200

    Update k3d

--- k3d_import_images.sh
+++ k3d_import_images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-k3d images import ghcr.io/cloudnative-pg/postgresql:14
-k3d images import ghcr.io/cloudnative-pg/postgresql:14.2
+k3d images import --mode direct ghcr.io/cloudnative-pg/postgresql:14
+k3d images import --mode direct ghcr.io/cloudnative-pg/postgresql:14.2
 #k3d images import ghcr.io/cloudnative-pg/postgresql:14.3
