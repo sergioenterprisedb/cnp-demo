674a06a51da2d40e3ade2bd5158161412399611e
Sergio Romera <sergio.romera@enterprisedb.com>
2022-06-03 13:56:49 +0200

    Update

--- 99_remove_cluster.sh
+++ 99_remove_cluster.sh
@@ -7,10 +7,15 @@ rm cluster-example.yaml
 # Uninstall operator
 kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/releases/cnpg-1.15.0.yaml
 
-# Delete cluster
+# K3d cluster
 k3d cluster delete
 k3d cluster create
-./docker_pull_images.sh
+
+#kind cluster
+kind delete cluster
+kind create cluster
+
+#./docker_pull_images.sh
 ./k3d_import_images.sh
 
 # Stop MinIO
