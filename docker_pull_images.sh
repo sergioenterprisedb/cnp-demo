f91965bb4463c6c890e977876cdeeb449a89e346
Sergio Romera <sergio.romera@enterprisedb.com>
2022-10-24 18:23:38 +0200

    Restore upgrade

--- docker_pull_images.sh
+++ docker_pull_images.sh
@@ -7,7 +7,7 @@ docker pull ghcr.io/cloudnative-pg/postgresql:14.2
 #docker pull ghcr.io/cloudnative-pg/postgresql:14.4
 docker pull ghcr.io/cloudnative-pg/postgresql:14.5
 #docker pull ghcr.io/cloudnative-pg/cloudnative-pg:1.15.0
-docker pull ghcr.io/cloudnative-pg/cloudnative-pg:1.17.0
+docker pull ghcr.io/cloudnative-pg/cloudnative-pg:1.17.1
 
 # Run postgres images
 #docker run --rm -e POSTGRES_PASSWORD=password ghcr.io/cloudnative-pg/postgresql:14
--- kind_import_images.sh
+++ kind_import_images.sh
@@ -3,7 +3,7 @@ kind load docker-image ghcr.io/cloudnative-pg/postgresql:14.2
 #kind load docker-image ghcr.io/cloudnative-pg/postgresql:14.3
 #kind load docker-image ghcr.io/cloudnative-pg/postgresql:14.4
 kind load docker-image ghcr.io/cloudnative-pg/postgresql:14.5
-kind load docker-image ghcr.io/cloudnative-pg/cloudnative-pg:1.17.0
+kind load docker-image ghcr.io/cloudnative-pg/cloudnative-pg:1.17.1
 
 # List images
 docker exec -it kind-control-plane crictl images
