fc273052a2a64b05babf4d8d87f44adad36c360e
Sergio Romera <sergio.romera@enterprisedb.com>
2023-06-06 12:23:51 +0200

    New features: PITR, fencing, replication slots

--- monitoring/99_remove_monitoring.sh
+++ monitoring/99_remove_monitoring.sh
@@ -2,10 +2,10 @@ kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-p
 kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/docs/src/samples/monitoring/cnpg-prometheusrule.yaml
 #kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/docs/src/samples/monitoring/kube-stack-config.yaml
 sleep 2
+helm uninstall prometheus-community
 kubectl delete crd prometheuses.monitoring.coreos.com
 kubectl delete crd prometheusagents.monitoring.coreos.com
 
-helm uninstall prometheus-community
 #helm uninstall prometheus-community/kube-prometheus-stack
 helm repo remove prometheus-community
 
