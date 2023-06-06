aac1fdd4bbea1070ea3f85a755408f4bd597d45d
Sergio Romera <sergio.romera@enterprisedb.com>
2023-06-06 09:18:32 +0200

    New features: PITR, fencing, replication slots

--- docker_pull_images.sh
+++ docker_pull_images.sh
@@ -5,6 +5,7 @@ docker pull ghcr.io/cloudnative-pg/postgresql:13
 docker pull ghcr.io/cloudnative-pg/postgresql:14.2
 docker pull ghcr.io/cloudnative-pg/postgresql:14.5
 docker pull ghcr.io/cloudnative-pg/cloudnative-pg:1.18.0
+docker pull ghcr.io/cloudnative-pg/cloudnative-pg:1.19.0
 
 # Run postgres images
 #docker run --rm -e POSTGRES_PASSWORD=password ghcr.io/cloudnative-pg/postgresql:14
