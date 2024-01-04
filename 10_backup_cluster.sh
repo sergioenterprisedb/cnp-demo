9fd86b5088bd09f7f4333d7dbe29c71213711433
Sergio Romera <sergio.romera@enterprisedb.com>
2024-01-04 16:04:53 +0100

    Bugfix

--- 10_backup_cluster.sh
+++ 10_backup_cluster.sh
@@ -5,3 +5,7 @@ kubectl delete -f backup.yaml
 
 printf "${green}kubectl apply -f backup.yaml${reset}\n"
 kubectl apply -f backup.yaml
+
+printf "${green}echo \"select pg_switch_wal()\" | kubectl-cnpg psql cluster-example${reset}\n"
+echo "select pg_switch_wal()" | kubectl-cnpg psql ${cluster_name} 
+
