7341db5be5c3ae433dbff3c98703bf8ec88c6314
Sergio Romera <sergio.romera@enterprisedb.com>
2024-02-20 08:11:51 +0100

    Version 1.0

---
+++ primary.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. ./config.sh
+
+export primary=`kubectl get pod -o=jsonpath="{range .items[*]}{.metadata.name}{'\t'}{.status.podIP}{'\t'}{.metadata.labels.role}{'\n'}" | grep ${cluster_name} | grep primary | awk '{print $1}'`
+printf "${yellow}Primary instance: ${red}${primary}${reset}\n"
+
