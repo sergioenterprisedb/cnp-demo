cf99cfa1b1c5bdb5752cfdf58f65f5e4a8a0ab3d
Sergio Romera <sergio.romera@enterprisedb.com>
2024-04-11 20:06:13 +0200

    Version 1.22 update

--- 02_install_operator.sh
+++ 02_install_operator.sh
@@ -6,8 +6,8 @@ version1=`kubectl cnpg version | awk '{ print $2 }' | awk -F":" '{ print $2}'`
 version2=${version1%??}
 #version1=1.20.0
 #version2=1.20
-printf "${green}kubectl apply -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-${version2}/releases/cnpg-${version1}.yaml${reset}\n"
+printf "${green}kubectl apply --server-side  --force-conflicts -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-${version2}/releases/cnpg-${version1}.yaml${reset}\n"
 
-kubectl apply -f \
+kubectl apply --server-side  --force-conflicts -f \
   https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-${version2}/releases/cnpg-${version1}.yaml
 
