1ee35f71758e07641bed94c561c8d9117b893ce7
Sergio Romera <sergio.romera@enterprisedb.com>
2024-05-15 16:49:35 +0200

    CPU's

--- k3d/create_cluster.sh
+++ k3d/create_cluster.sh
@@ -6,3 +6,10 @@ k3d cluster create test --agents 4
 # Create 3 servers (3 nodes with 1 control plane each)
 #k3d cluster create test --servers 3
 
+# Taint node
+./taint.sh
+
+# Check taints
+kubectl get nodes -o go-template='{{printf "%-50s %-12s\n" "Node" "Taint"}}{{- range .items}}{{- if $taint := (index .spec "taints") }}{{- .metadata.name }}{{ "\t" }}{{- range $taint }}{{- .key }}={{ .value }}:{{ .effect }}{{ "\t" }}{{- end }}{{- "\n" }}{{- end}}{{- end}}'
+
+
--- upgrades/monitoring_pod.sh
+++ upgrades/monitoring_pod.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Define colors
+RED='\e[31m'
+GREEN='\e[32m'
+NC='\e[33m' # No Color
+
 kubectl_filter="\
 {range .items[*]}{.metadata.name}{','}\
 {.metadata.labels.cnpg\.io\/cluster}{','}\
@@ -12,6 +17,8 @@ kubectl_filter="\
 echo "Instance Name,Cluster Name,Status,Image Version,Role,Node name,Operator Version" > ./monitor.log
 
 kubectl get pod -o=jsonpath="$kubectl_filter"  >> ./monitor.log
+
+
 cat monitor.log | sort | column -s, -t
 
 
