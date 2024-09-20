c1650386be162901125e92de7005f8fd7329189b
Sergio Romera <sergio.romera@enterprisedb.com>
2024-09-20 17:19:37 +0200

    bugfix

--- 13_failover.sh
+++ 13_failover.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 . ./config.sh
 
-#kubectl delete pod cluster-example-2 --force
-primary=`kubectl get pod -o=jsonpath="{range .items[*]}{.metadata.name}{'\t'}{.status.podIP}{'\t'}{.metadata.labels.role}{'\n'}" | grep primary | awk '{print $1}'`
-printf "${yellow}Primary instance: ${red}${primary}${reset}\n"
+. ./primary.sh
+
 printf "${yellow}Deleting pvc and pod from primary instance ${primary}...${reset}\n"
 
-#kubectl delete pvc/cluster-example-2 pod/cluster-example-2 --force
 printf "${green}kubectl delete pvc/${primary} pod/${primary} --force${reset}\n"
-#kubectl delete pvc/${primary} pod/${primary} --force
 kubectl delete pvc/${primary} pod/${primary} pvc/${primary}-tbs-idx pvc/${primary}-tbs-tmptbs pvc/${primary}-wal --force
-#kubectl delete pod cluster-example-2 --force
 
