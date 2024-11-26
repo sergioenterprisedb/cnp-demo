1b83cbe86c04d3854a7ba7665ca4e8673c3b84a4
Sergio Romera <sergio.romera@enterprisedb.com>
2024-11-26 14:06:05 +0100

    tde

--- tde/01_install_operator.sh
+++ tde/01_install_operator.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.0.yaml
-kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.1.yaml
-kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.2.yaml
+#kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.0.yaml
+#kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.1.yaml
+#kubectl delete -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.23/releases/cnpg-1.23.2.yaml
 
-kubectl apply --force-conflicts --server-side -f https://get.enterprisedb.io/cnp/postgresql-operator-1.23.2.yaml
-kubectl get deployment -n postgresql-operator-system postgresql-operator-controller-manager
+# Create namespace
+kubectl create namespace postgresql-operator-system
+
+. ../config.sh
+. ./.credentials.sh
+
+docker login docker.enterprisedb.com -u $EDB_SUBSCRIPTION_PLAN -p $EDB_SUBSCRIPTION_TOKEN
+
+version1=`kubectl cnpg version | awk '{ print $2 }' | awk -F":" '{ print $2}'`
+version2=${version1%??}
+printf "${green}kubectl apply --force-conflicts --server-side -f https://get.enterprisedb.io/cnp/postgresql-operator-${version1}.yaml${reset}\n"
+
+kubectl create secret -n postgresql-operator-system docker-registry edb-pull-secret \
+  --docker-server=docker.enterprisedb.com \
+  --docker-username=k8s_$EDB_SUBSCRIPTION_PLAN \
+  --docker-password=$EDB_SUBSCRIPTION_TOKEN
+
+sleep 5
+
+#kubectl apply --force-conflicts --server-side -f https://get.enterprisedb.io/cnp/postgresql-operator-${version1}.yaml
+kubectl apply --server-side -f \
+   https://get.enterprisedb.io/pg4k/pg4k-$EDB_SUBSCRIPTION_PLAN-${version1}.yaml
+
+sleep 15
+#kubectl get deployment -n postgresql-operator-system postgresql-operator-controller-manager
+kubectl get deployments.apps -n postgresql-operator-system
 
--- tde/99_delete_operator.sh
+++ tde/99_delete_operator.sh
@@ -5,6 +5,17 @@ kubectl delete cluster cluster-example
 kubectl delete -f https://get.enterprisedb.io/cnp/postgresql-operator-1.23.0.yaml
 kubectl delete -f https://get.enterprisedb.io/cnp/postgresql-operator-1.23.1.yaml
 kubectl delete -f https://get.enterprisedb.io/cnp/postgresql-operator-1.23.2.yaml
+kubectl delete -f https://get.enterprisedb.io/cnp/postgresql-operator-1.24.1.yaml
 kubectl delete secret tde-key
 rm tde_file.log
+
+. ../config.sh
+. ./.credentials
+
+version1=`kubectl cnpg version | awk '{ print $2 }' | awk -F":" '{ print $2}'`
+version2=${version1%??}
+kubectl delete -f --force-conflicts --server-side -f https://get.enterprisedb.io/cnp/postgresql-operator-${version1}.yaml
+
+kubectl delete -f \
+   https://get.enterprisedb.io/pg4k/pg4k-$EDB_SUBSCRIPTION_PLAN-${version1}.yaml
 
