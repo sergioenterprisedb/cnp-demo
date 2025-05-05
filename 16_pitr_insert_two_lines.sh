d9347558941dfe86d93b4261ffc52af3cfe28f10
Sergio Romera <sergio.romera@enterprisedb.com>
2025-05-05 09:48:09 +0200

    Doc

--- 16_pitr_insert_two_lines.sh
+++ 16_pitr_insert_two_lines.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 . ./config.sh
+
+echo 16 > ./docs/docid
+
 printf "${green}kubectl exec -it cluster-example-1 -- psql < ./pitr/pitr_insert_data_1.sql${reset}\n"
 
 kubectl exec -it cluster-example-1 -- psql  < ./pitr/pitr_insert_data_1.sql
